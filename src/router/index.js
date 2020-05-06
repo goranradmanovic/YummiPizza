d7a1b91e2b2f7443549a1c967980aeda2e655fb8
Goran Radmanovic <goranradmanovic@gmail.com>
2020-05-06 15:28:54 +0200

    Adding protected route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import store from '../store'
 import Home from '../views/Home.vue'
+import AllOrders from '../views/AllOrders.vue'
 
 Vue.use(VueRouter)
 
@@ -10,19 +12,24 @@ Vue.use(VueRouter)
     name: 'Home',
     component: Home
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
 
   {
     path: '/menu',
     name: 'Menu',
     component: () => import(/* webpackChunkName: "menu" */ '../views/Menu.vue')
+  },
+
+  {
+    path: '/allorders',
+    name: AllOrders,
+    component: AllOrders,
+    beforeEnter: (to, from, next) => {
+      if (store.getters.loggedIn) {
+        next();
+      } else {
+        next(false);
+      }
+    }
   }
 ]
 
