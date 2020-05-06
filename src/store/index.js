0370c6153438ef37a01707309c9b18cd3c98fd1e
Goran Radmanovic <goranradmanovic@gmail.com>
2020-05-06 15:31:35 +0200

    Adding new store code

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,39 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    loggedIn: false,
+    currentRate: 1.65
   },
   mutations: {
+    setLogginStatus(state, data) {
+      state.loggedIn = data;
+    },
+
+    setCurrentRate(state, data) {
+      state.currentRate = data;
+    },
   },
   actions: {
+    userLoggedIn({ commit }, data) {
+      commit("setLogginStatus", data);
+    },
+
+    userLoggedOut({ commit }, data) {
+      commit("setLogginStatus", data);
+    },
+
+    currentRateExcange({ commit }, data) {
+      commit("setCurrentRate", data);
+    },
+  },
+  getters: {
+    loggedIn(state) {
+      return state.loggedIn;
+    },
+
+    getCurrentRate(state) {
+      return state.currentRate;
+    }
   },
   modules: {
   }
